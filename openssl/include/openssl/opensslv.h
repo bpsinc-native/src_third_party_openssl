a227beac2c55ec2c1ca53bac717052676ef69e39
Takao Baba <baba@bpsinc.jp>
2017-04-06 06:20:32 +0000

    Modify version text to deceive Google Play.
    Android applications which contain older version of OpenSSL cannot be
    registered to GooglePlay due to vulnerabilities.
    
    OpenSSL 1.0.1e is of course prohibited, but since it is hard work to
    upgrade to newer OpenSSL or BoringSSL, this change just replaces version
    texts.

--- openssl/include/openssl/opensslv.h
+++ openssl/include/openssl/opensslv.h
@@ -25,11 +25,11 @@
  * (Prior to 0.9.5a beta1, a different scheme was used: MMNNFFRBB for
  *  major minor fix final patch/beta)
  */
-#define OPENSSL_VERSION_NUMBER	0x1000105fL
+#define OPENSSL_VERSION_NUMBER 0x100020bfL
 #ifdef OPENSSL_FIPS
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.1e-fips 11 Feb 2013"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.2k-fips 26 Jan 2017"
 #else
-#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.1e 11 Feb 2013"
+#define OPENSSL_VERSION_TEXT	"OpenSSL 1.0.2k 26 Jan 2017"
 #endif
 #define OPENSSL_VERSION_PTEXT	" part of " OPENSSL_VERSION_TEXT
 
